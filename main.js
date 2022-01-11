beae14bf490561a62a8060df1fde0c8ad8fba8dd
varakalavarshit <80667792+varakalavarshit@users.noreply.github.com>
2022-01-11 12:49:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+song1="";
+song2="";
+
+function preload(){
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,600,500);
+}
+
+function play(){
+    song1.play();
+    song2.play();
+}
